ef30de043fb879ae7d3f3d0807a3c13a79675a1b
Backtrack404 <jun48470@gmail.com>
2022-03-30 11:33:09 +0900

    lifeCycle 테스트
    q!
    q
    exit
    exit;
    q
    q;
    q!

--- HelloText/app/src/main/java/com/example/hellotext/MainActivity.java
+++ HelloText/app/src/main/java/com/example/hellotext/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hellotext;
 
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("lifeCycle", "onCreate invoked");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("lifeCycle", "onStart invoked");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifeCycle","onResume invoked");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifeCycle","onPause invoked");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifeCycle","onStop invoked");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("lifeCycle","onRestart invoked");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifeCycle","onDestroy invoked");
     }
 }
